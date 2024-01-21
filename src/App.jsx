8bf906197037212ed344b5dc89b2ee241ee0c9e8
Roy Ledford <bluesixty@gmail.com>
2024-01-21 11:33:58 -0500

    fix prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { use } from "chai";
-import AuthProvider from "./providers/AuthProvider"
-import Routes from "./routes";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import { use } from 'chai'
+import AuthProvider from './providers/AuthProvider'
+import Routes from './routes'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 
 const POSTS = [
-  { id: 1, title: "Post 1" },
-  { id: 2, title: "Post 2" },
-];
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' },
+]
 
 export default function App() {
-
   const postsQuery = useQuery({
-    queryKey: ["posts"],
-    queryFn:  () => wait(1000).then(() => [...POSTS])
+    queryKey: ['posts'],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
   })
-
 
   return (
     // <AuthProvider>
@@ -25,10 +23,9 @@ export default function App() {
         <div key={post.id}>{post.title}</div>
       ))}
     </div>
-
-  );
+  )
 }
 
 function wait(duration) {
-  return new Promise((resolve) => setTimeout(resolve, duration));
+  return new Promise((resolve) => setTimeout(resolve, duration))
 }
